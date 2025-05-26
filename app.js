b986c25b73252bc1b58bdfe73bfd2b496ac47467
Johannes Ahrndt <ahrndt@webhub.de>
2025-05-26 10:41:00 +0200

    WIP: Hero slider

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import './css/app.css'
 import initTooltips from './components/atoms/tooltip/tooltip.js'
 import initNavigationBars from './components/molecules/navigation-bar/navigation-bar.js'
+import initHeroSliders from './components/organisms/hero-slider/hero-slider.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     initNavigationBars();
+    initHeroSliders();
     initTooltips();
 })
