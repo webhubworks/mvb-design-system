2235cf7ed3c881a344c3b6545002a007d5a66839
Marten Brosch <brosch@webhub.de>
2025-05-28 15:27:04 +0200

    Added: testimonial slider

---
+++ components/organisms/testimonial-slider/testimonial-slider.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+
+const initTestimonialSlider = component => {
+    const nextButton = component.querySelector('[data-component-part="testimonial-slider.next"]')
+    const prevButton = component.querySelector('[data-component-part="testimonial-slider.prev"]')
+    const playButton = component.querySelector('[data-component-part="testimonial-slider.play"]')
+    const playButtonIcon = playButton.querySelector('[data-component="atoms.icon"]')
+    const swiper = new Swiper(component, {
+        modules: [Navigation],
+        a11y: true,
+        autoHeight: true,
+        loop: true,
+        autoplay: {
+            delay: 300,
+            pauseOnMouseEnter: false,
+        },
+    })
+    if (nextButton) {
+        nextButton.addEventListener('click', () => {
+            swiper.slideNext()
+        })
+    }
+    if (prevButton) {
+        prevButton.addEventListener('click', () => {
+            swiper.slidePrev()
+        })
+    }
+    if (playButton) {
+        playButton.addEventListener('click', () => {
+            playButtonIcon.classList.toggle('fa-play-circle')
+            playButtonIcon.classList.toggle('fa-pause-circle')
+            playButton.setAttribute('aria-label', 'Play slideshow')
+            playButton.setAttribute('aria-label', 'Pause slideshow')
+            // ToDo: Implement play/pause functionality
+
+            swiper.autoplay.running ? swiper.autoplay.stop() : swiper.autoplay.start()
+        })
+    }
+}
+
+const initTestimonialSliders = () => {
+    document.querySelectorAll('[data-component="organisms.testimonial-slider"]').forEach((component) => {
+        initTestimonialSlider(component)
+    })
+}
+
+export default initTestimonialSliders
--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ import initTooltips from '@components/atoms/tooltip/tooltip.js'
 import initNavigationBars from '@components/molecules/navigation-bar/navigation-bar.js'
 import initHeroSliders from '@components/organisms/hero-slider/hero-slider.js'
 import initLanguageSwitchers from '@components/molecules/language-switcher/language-switcher.js'
+import initTestimonialSliders from "@components/organisms/testimonial-slider/testimonial-slider.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     initLanguageSwitchers();
     initTooltips();
     initNavigationBars();
     initHeroSliders();
+    initTestimonialSliders()
 })
