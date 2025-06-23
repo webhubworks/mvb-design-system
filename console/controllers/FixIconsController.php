abd1b099ff923f09119255747320d9ec58c5107f
Marten Brosch <brosch@webhub.de>
2025-06-23 15:17:28 +0200

    added fix icons console command

---
+++ console/controllers/FixIconsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace webhubworks\mvbdesignsystem\console\controllers;
+
+use Craft;
+use craft\console\Controller;
+use craft\elements\Entry;
+
+use yii\console\ExitCode;
+
+class FixIconsController extends Controller
+{
+    public function actionIndex(): int
+    {
+        $entries = Entry::find()
+            ->anyStatus()
+            ->all();
+
+        foreach ($entries as $entry) {
+            $dirty = false;
+
+            foreach ($entry->getFieldValues() as $handle => $value) {
+                if (is_string($value)) {
+                    $new = preg_replace('/fa[blsr] fa-/', '', $value);
+                    if ($new !== $value) {
+                        $entry->setFieldValue($handle, $new);
+                        $dirty = true;
+                    }
+                }
+            }
+
+            if ($dirty) {
+                if (!Craft::$app->elements->saveElement($entry)) {
+                    echo "Fehler beim Speichern von Eintrag: {$entry->title}\n";
+                } else {
+                    echo "Icon-KLassen aktualisiert in {$entry->title}\n";
+                }
+            }
+        }
+
+        return ExitCode::OK;
+    }
+}
