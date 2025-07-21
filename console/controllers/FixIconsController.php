676a9c8f772ac28ab35705e15811e06e5f127a69
Marten Brosch <brosch@webhub.de>
2025-07-21 10:53:23 +0200

    fix icon command + nullable breadcrumb

--- console/controllers/FixIconsController.php
+++ console/controllers/FixIconsController.php
@@ -12,28 +12,27 @@ class FixIconsController extends Controller
 {
     public function actionIndex(): int
     {
-        $entries = Entry::find()
-            ->anyStatus()
-            ->all();
+        $entries = Entry::find()->status(null)->batch(100);
+        foreach ($entries as $batch) {
+            foreach ($batch as $entry) {
+                $dirty = false;
 
-        foreach ($entries as $entry) {
-            $dirty = false;
-
-            foreach ($entry->getFieldValues() as $handle => $value) {
-                if (is_string($value)) {
-                    $new = preg_replace('/fa[blsr] fa-/', '', $value);
-                    if ($new !== $value) {
-                        $entry->setFieldValue($handle, $new);
-                        $dirty = true;
+                foreach ($entry->getFieldValues() as $handle => $value) {
+                    if (is_string($value)) {
+                        $new = preg_replace('/fa[blsr] fa-/', '', $value);
+                        if ($new !== $value) {
+                            $entry->setFieldValue($handle, $new);
+                            $dirty = true;
+                        }
                     }
                 }
-            }
 
-            if ($dirty) {
-                if (!Craft::$app->elements->saveElement($entry)) {
-                    echo "Fehler beim Speichern von Eintrag mit der ID: {$entry->id}\n";
-                } else {
-                    echo "Icon-KLassen bereinigt\n";
+                if ($dirty) {
+                    if (!Craft::$app->elements->saveElement($entry)) {
+                        echo "Fehler beim Speichern von Eintrag mit der ID: {$entry->id}\n";
+                    } else {
+                        echo "Icon-KLassen bereinigt\n";
+                    }
                 }
             }
         }
