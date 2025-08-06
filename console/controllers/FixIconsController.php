e26645712810f57e51ed3097574d613286b1ab7a
Marten Brosch <brosch@webhub.de>
2025-08-06 08:46:38 +0200

    improved fix icons command

--- console/controllers/FixIconsController.php
+++ console/controllers/FixIconsController.php
@@ -5,7 +5,6 @@ namespace webhubworks\mvbdesignsystem\console\controllers;
 use Craft;
 use craft\console\Controller;
 use craft\elements\Entry;
-
 use yii\console\ExitCode;
 
 class FixIconsController extends Controller
@@ -18,12 +17,26 @@ class FixIconsController extends Controller
                 $dirty = false;
 
                 foreach ($entry->getFieldValues() as $handle => $value) {
-                    if (is_string($value)) {
-                        $new = preg_replace('/fa[blsr] fa-/', '', $value);
-                        if ($new !== $value) {
-                            $entry->setFieldValue($handle, $new);
-                            $dirty = true;
+                    if (!is_string($value)) {
+                        continue;
+                    }
+
+                    // 1. Schritt: Entferne alle "fa[blsr] fa-"
+                    $new = preg_replace('/fa[blsr]\s+fa-/', '', $value);
+
+                    // 2. Schritt: Wenn sich nichts geändert hat, extrahiere das zweite "fa-..."
+                    if ($new === $value) {
+                        // Suche alle Klassen "fa-..."
+                        if (preg_match_all('/\bfa-([^\s]+)/', $value, $matches) && count($matches[1]) >= 2) {
+                            // Das zweite Vorkommen (Index 1) übernehmen
+                            $new = $matches[1][1];
                         }
+                    }
+
+                    // Falls wir etwas geändert haben, Wert setzen
+                    if ($new !== $value) {
+                        $entry->setFieldValue($handle, $new);
+                        $dirty = true;
                     }
                 }
 
